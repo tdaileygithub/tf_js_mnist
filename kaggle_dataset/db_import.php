68a6ee80b7141f9222374b5e09649c86bd649ebc
timothy.j.dailey@gmail.com <timothy.j.dailey@gmail.com>
2019-01-12 14:50:05 -0800

    db_import - write PNG as blob column -- add predict column

--- kaggle_dataset/db_import.php
+++ kaggle_dataset/db_import.php
@@ -14,40 +14,67 @@ $db->exec('
 CREATE TABLE `images` (
 	`id`			INTEGER PRIMARY KEY AUTOINCREMENT,	
 	`label`			INTEGER NULL CHECK(label >= 0 and label <= 9),
-	`prediction`	INTEGER NULL CHECK(prediction >= 0 and prediction <= 9),
-	`pixels`		TEXT NOT NULL
+	`prediction`	INTEGER NULL CHECK(prediction >= 0 and prediction <= 9),	
+	`pixels`		BLOB NULL
 )
 ');
 
+function write_png_from_pixels($pixels_arr) {
+	$im 	= imagecreatetruecolor(28, 28);
+	$white 	= imagecolorallocate($im, 255, 255, 255);
+	$black 	= imagecolorallocate($im, 0, 0, 0);	  
+
+	$index = 0;
+	for ($row = 0; $row < 28; $row++) {
+		for ($col = 0; $col < 28; $col++) {
+			$pix = $pixels_arr[$index];
+			if (0 == $pix) {
+				imagesetpixel($im, $col, $row, $black);
+			} else {
+				imagesetpixel($im, $col, $row, $white);
+			}
+			$index++;
+		}
+	}		   
+	 $path = 'temp.png';
+	 imagepng ($im, $path,0,NULL);	 
+	 $bytes = file_get_contents($path);
+	 unlink($path);
+	 return $bytes;
+	 
+}
+
 $file = fopen("train.csv","r");
-$row = 0;
+$row_ct = 0;
 $db->exec('BEGIN;');
 while (($data = fgetcsv($file)) !== FALSE) {
-	if ($row > 0) {
+	if ($row_ct > 0) {
 		$label_val = $data[0];
+		 
 		$stmt = $db->prepare('INSERT INTO images (label,pixels) VALUES (:label,:pixels)');		
-		$stmt->bindValue(':label',			$data[0],								SQLITE3_INTEGER);
-		$stmt->bindValue(':pixels',			implode(",", array_slice($data, 1)),	SQLITE3_TEXT);
+		$stmt->bindValue(':label',			$data[0],								SQLITE3_INTEGER);		
+		$stmt->bindValue(':pixels', 		write_png_from_pixels(array_slice($data, 1)), 				\PDO::PARAM_LOB);
 		$stmt->execute();
-		echo "row: " . $row . "\n";
+
+		echo "row: " . $row_ct . "\n";
 	}
-	$row++;
+	$row_ct++;
 }
 $db->exec('COMMIT;');
 fclose($file);
 
 $file = fopen("test.csv","r");
-$row = 0;
+$row_ct = 0;
 $db->exec('BEGIN;');
 while (($data = fgetcsv($file)) !== FALSE) {
-	if ($row > 0) {		
+	if ($row_ct > 0) {
 		$stmt = $db->prepare('INSERT INTO images (label,pixels) VALUES (:label,:pixels)');		
-		$stmt->bindValue(':label',			NULL,									SQLITE3_INTEGER);
-		$stmt->bindValue(':pixels',			implode(",", array_slice($data, 0)),	SQLITE3_TEXT);
+		$stmt->bindValue(':label',			NULL,									SQLITE3_INTEGER);		
+		$stmt->bindValue(':pixels', 		write_png_from_pixels(array_slice($data, 0)), 				\PDO::PARAM_LOB);
 		$stmt->execute();
-		echo "row: " . $row . "\n";
+		echo "row: " . $row_ct . "\n";		
 	}
-	$row++;
+	$row_ct++;
 }
 $db->exec('COMMIT;');
 fclose($file);
